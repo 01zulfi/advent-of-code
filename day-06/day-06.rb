f04ab685e35092d18f0db23a4e5792c91c9a99ac
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-12-06 17:20:32 +0500

    Complete day 6

---
+++ day-06/day-06.rb
@@ -0,0 +1,12 @@
+file_path = ARGV[0]
+
+datastream = File.read(file_path)
+
+def first_marker(datastream, distint_chars)
+  datastream.chars.each_cons(distint_chars).with_index do |chars, i|
+    return i + distint_chars if chars.uniq.length == chars.length
+  end
+end
+
+p "Part one: #{first_marker(datastream, 4)}"
+p "Part two: #{first_marker(datastream, 14)}"
